38f961d48099a36d7ca6a02f48fdde94e168d5f1
Pavel Likhopoy <plikhopoy@team.amocrm.com>
2022-01-21 15:12:42 +0300

    \Factory\TestHandlerFactpory

---
+++ src/App/Handler/TestHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Handler;
+
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\ServerRequestInterface;
+use Psr\Http\Server\RequestHandlerInterface;
+use Laminas\Diactoros\Response\HtmlResponse;
+use Mezzio\Template\TemplateRendererInterface;
+use App\TestHandlerFactory;
+
+class TestHandler implements RequestHandlerInterface
+{
+    /**
+     * @var TemplateRendererInterface
+     */
+    private $renderer;
+    private $config;
+
+    public function __construct(TemplateRendererInterface $renderer, $config)
+    {
+        $this->renderer = $renderer;
+        $this->config = $config;
+    }
+
+    public function handle(ServerRequestInterface $request) : ResponseInterface
+    {
+        // Do some work...
+        // Render and return a response:
+        $data['test'] = $this->config['clientId'];
+        return new HtmlResponse($this->renderer->render(
+            'app::test-handler',
+            //[] // parameters to pass to template
+            $data
+        ));
+    }
+}
