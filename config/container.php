69ebeabc1fdcf7abbf1cb4d7e338c87a7b7f2b40
Pavel Likhopoy <plikhopoy@team.amocrm.com>
2022-01-26 17:51:29 +0300

    phpmig ok

--- config/container.php
+++ config/container.php
@@ -11,11 +11,11 @@ $config = require __DIR__ . '/config.php';
 $dependencies                       = $config['dependencies'];
 $dependencies['services']['config'] = $config;
 
-$capsule = new Manager();
-$capsuleContainer = $capsule->getContainer();
-$capsule->addConnection($config['database']);
-$capsule->setAsGlobal();
-$capsule->bootEloquent();
+// $capsule = new Manager();
+// $capsuleContainer = $capsule->getContainer();
+// $capsule->addConnection($config['database']);
+// $capsule->setAsGlobal();
+// $capsule->bootEloquent();
 
 // Build container
 return new ServiceManager($dependencies);
